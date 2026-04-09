f88e5d5dfc947cb8e42be56ed90cd073636d5bb2
kelvintee <1440531680@qq.com>
2026-04-09 20:51:12 +0800

    Initial Commit

---
+++ SafeEat/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SafeEat
+//
+//  Created by Tee on 2026/4/9.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SafeEat/SafeEatApp.swift
@@ -0,0 +1,32 @@
+//
+//  SafeEatApp.swift
+//  SafeEat
+//
+//  Created by Tee on 2026/4/9.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SafeEatApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
